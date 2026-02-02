44c7b77b1c26af60c6f33dd41e54ba841e25ddcd
ficprashantb <prashantb@first-insight.com>
2026-02-02 18:44:01 +0530

    02 Feb 2026 3

--- Test Suites/EVAA Scribe Upload Test Suite.ts
+++ Test Suites/EVAA Scribe Upload Test Suite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>EVAA Upload Test Suite</name>
+   <name>EVAA Scribe Upload Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
